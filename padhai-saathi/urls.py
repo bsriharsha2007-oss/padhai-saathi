3a2c6c2b510346724ad5b77e5fcceef0e8a77bf3
SRI HARSHA <bsriharsha2007@gmail.com>
2025-12-27 18:19:02 +0530

    Update padhai-saathi/urls.py

--- padhai-saathi/urls.py
+++ padhai-saathi/urls.py
@@ -1,4 +1,4 @@
-from contextlib import redirect_stderr
+from django.shortcuts import redirect
 from django.urls import path
 from . import views
 
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('flashcards/<int:class_num>/<str:subject_name>/<str:chapter>/', views.flashcards_view, name='flashcards'),
     path('quiz/<str:chapter>/', views.quiz_view, name='quiz'),
-    path('result/', lambda r: redirect_stderr('home')),
+    path('result/', lambda r: redirect('home')),
     path('qr/<int:class_num>/<str:subject_name>/<str:chapter>/', views.qr_formula, name='qr_formula'),
     path('leaderboard/', views.leaderboard, name='leaderboard'),
     path('whatsapp/webhook/', views.whatsapp_webhook, name='whatsapp_webhook'),
